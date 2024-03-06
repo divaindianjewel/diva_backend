50d43214d26e6153f222357791b35733f0982fa4
= <=>
2024-03-07 00:50:08 +0530

    some changes in production level

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,20 +1,17 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-     default: {
-       connector: 'mongoose',
-       settings: {
-         host: env('DATABASE_HOST', 'db-postgresql-nyc3-57037-do-user-15972844-0.c.db.ondigitalocean.com'),
-         port: env.int('DATABASE_PORT', 25060),
-         database: env('DATABASE_NAME', 'defaultdb'),
-         username: env('DATABASE_USERNAME', 'doadmin'),
-         password: env('DATABASE_PASSWORD', 'AVNS_JXmo4yxFdkYcfq9SUgT'),
-       },
-       options: {
-         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-         ssl: env.bool('DATABASE_SSL', true),
-       },
-     },
+
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'), 
+      port: env.int('DATABASE_PORT'), 
+      database: env('DATABASE_NAME'), 
+      user: env('DATABASE_USERNAME'), 
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
   },
- });
- 
+});
